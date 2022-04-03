bd47f67246dadf203ad443a724e2917dcf9e84c5
Owen Buckley <owenbuckley13@gmail.com>
2022-04-03 19:58:18 -0400

    basic page based rendering with declarative shadow dom

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # wcc
 
 Experimental native Web Components compiler.
+
+> _**Make sure to test in Chrome, or other Declarative Shadow DOM compatible browser.**_
--- server.js
+++ server.js
@@ -1,27 +1,41 @@
 import fastify from 'fastify';
+import fastifyStatic from 'fastify-static';
 import { renderToString } from './lib/wcc.js';
 import HomePage from './www/index.js';
 
 const app = fastify({ logger: true });
 
-// Declare a route
+app.register(fastifyStatic, {
+  root: new URL('./www', import.meta.url).pathname,
+})
+
 app.get('/', async (request, reply) => {
   const page = await renderToString(HomePage);
-  // console.debug(renderToString(HomePage));
 
   reply
     .header('Content-Type', 'text/html; charset=utf-8')
     .send(`
       <html>
+        <head>
+          <title>WCC</title>
+        </head>
         <body>
           <h1>Hello World</h1>
-          ${page}
+
+          <page-entry>
+            ${page}
+          </page-entry>
+
+          <script type="module">
+            import PageEntry from './index.js';
+
+            customElements.define('page-entry', PageEntry);
+          </script>
         </body>
       </html>
     `);
 });
 
-// Run the server!
 const start = async () => {
   try {
     await app.listen(3000)
