a4193b91eb9d77595e260f5bc4c5f1d5770107ce
Owen Buckley <owenbuckley13@gmail.com>
2022-04-10 11:31:50 -0400

    init SSR data loading mechanics

---
+++ www/pages/artists.js
@@ -0,0 +1,74 @@
+import '../components/footer.js';
+import '../components/header.js';
+
+export default class ArtistsPage extends HTMLElement {
+  constructor(data = {}) {
+    super();
+
+    console.debug('ArtistsPage constructor', this.shadowRoot)
+
+    if(this.shadowRoot) {
+      console.debug('ArtistsPage => shadowRoot detected!')
+      const artists = JSON.parse(this.shadowRoot.querySelector('script[type="application/json"').text);
+      console.log("Found SSR artists with length", artists.length);
+      this.artists = artists;
+    } else {
+      this.attachShadow({ mode: 'open' });
+      console.debug(this.shadowRoot);
+      this.artists = data.artists || [];
+    }
+
+    console.debug('=====================');
+  }
+
+  connectedCallback() {
+    this.shadowRoot.innerHTML = this.getTemplate();
+  }
+
+  getTemplate() {
+    return `
+      <script type="application/json">
+        ${JSON.stringify(this.artists)}
+      </script>
+
+      <wcc-header></wcc-header>
+
+      <h1>Artists Page</h1>
+
+      <table>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Image</td>
+          </tr>
+        </thead>
+        <tbody>
+          ${this.artists.map(artist => {
+            const { name, imageUrl } = artist;
+
+            return `
+              <tr>
+                <td><h1>${name}</h1></td>
+                <td><img src="${imageUrl}" alt="${name}" loading="lazy"/></td>
+              </tr>
+            `;
+          }).join('\n')}
+        </tbody>
+      </table>
+
+      <wcc-footer></wcc-footer>
+    `;
+  }
+}
+
+export async function getData() {
+  return {
+    artists:   [
+      { id: "1", name: "Analog", imageUrl: "//d34k5cjnk2rcze.cloudfront.net/images/artists/analog.jpg" },
+      { id: "2", name: "Electro Calrissian", imageUrl: "//d34k5cjnk2rcze.cloudfront.net/images/artists/electro-calrissian.jpg" },
+      { id: "3", name: "Rory Boyan", imageUrl: "//d34k5cjnk2rcze.cloudfront.net/images/artists/rory.jpg" }
+    ]
+  }
+}
+
+// customElements.define('page-entry', ArtistsPage)
