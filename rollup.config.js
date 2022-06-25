bd7f58a1daf247a75b63375f321b84340a4f1f43
Owen Buckley <owenbuckley13@gmail.com>
2022-06-24 20:40:54 -0400

    handling for type module packages

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import commonjs from '@rollup/plugin-commonjs';
 export default {
   input: 'src/wcc.js',
   output: {
-    file: 'dist/wcc.dist.js',
+    file: 'dist/wcc.dist.cjs',
     format: 'cjs'
   },
   plugins: [
