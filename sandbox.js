128d95db49ec073e7e44f0ffa541ad3ee00b4fb7
Owen Buckley <owenbuckley13@gmail.com>
2022-07-04 14:14:21 -0400

    sandbox dev workflow and counter control example

--- sandbox.js
+++ sandbox.js
@@ -16,30 +16,38 @@ async function init() {
       </script>
     `;
   });
+  const control = await fs.readFile(new URL('./test/cases/jsx/src/counter-control.js', import.meta.url), 'utf-8');
 
   await fs.rm(sandboxRoot, { recursive: true, force: true });
   await fs.mkdir(sandboxRoot, { recursive: true });
-  await fs.writeFile(new URL(`${sandboxRoot}//index.html`, import.meta.url), `
+  await fs.writeFile(new URL(`${sandboxRoot}/index.html`, import.meta.url), `
     <!DOCTYPE html>
     <html lang="en" prefix="og:http://ogp.me/ns#">
 
       <head>
         <title>WCC - Sandbox</title>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <script type="module">
+          ${control}
+        </script>
         ${sources.join('\n')}
       </head>
 
       <body>
         <h1>WCC Sandbox</h1>
 
-        ${tags.join('\n')}
+        <h2>Control Group</h2>
+        <wcc-counter-control></wcc-counter-control>
 
         <hr/>
 
+        <h2>Sources</h2>
+
+        ${tags.join('\n')}
         ${Object.keys(metadata).map(key => `<pre>${metadata[key].source}</pre>`).join('\n')}
       </body>
     </html>
-  `.trim());
+  `);
 }
 
 init();
