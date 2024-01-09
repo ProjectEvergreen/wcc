778183aa3fef954b721ebfa4e6079414609037aa
Owen Buckley <owenbuckley13@gmail.com>
2024-01-09 18:56:11 -0500

    add attribute to inferredObservablity test case

--- test/cases/jsx-inferred-observability/src/counter.jsx
+++ test/cases/jsx-inferred-observability/src/counter.jsx
@@ -4,6 +4,7 @@ export default class Counter extends HTMLElement {
   constructor() {
     super();
     this.count = 0;
+    this.highlight = 'red';
   }
 
   increment() {
@@ -21,28 +22,16 @@ export default class Counter extends HTMLElement {
   }
 
   render() {
-    const { count } = this;
+    const { count, highlight } = this;
 
     return (
       <div>
         <wcc-badge count={count}></wcc-badge>
         <h3 data-test="hello123">Counter JSX</h3>
-        <button id="evt-this" onclick={this.decrement}>
-          {' '}
-          - (function reference)
-        </button>
-        <button id="evt-assignment" onclick={(this.count -= 1)}>
-          {' '}
-          - (inline state update)
-        </button>
-        <span>
-          You have clicked{' '}
-          <span class="red" id="expression">
-            {count}
-          </span>{' '}
-          times
-        </span>
-        <button onclick={(this.count += 1)}> + (inline state update)</button>
+        <button id="evt-this" onclick={this.decrement}> -  (function reference)</button>
+        <button id="evt-assignment" onclick={this.count -= 1}> - (inline state update)</button>
+        <span>You have clicked <span class={highlight} id="expression">{count}</span> times</span>
+        <button onclick={this.count += 1}> + (inline state update)</button>
         <button onclick={this.increment}> + (function reference)</button>
       </div>
     );
