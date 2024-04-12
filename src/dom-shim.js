f2fddfb67fb55528d0a95842e2e318d348f7d322
Owen Buckley <owenbuckley13@gmail.com>
2024-04-12 19:54:48 -0400

    document CSSStyleSheet support in DOM shim

--- src/dom-shim.js
+++ src/dom-shim.js
@@ -2,7 +2,8 @@ function noop() { }
 
 // https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleSheet/CSSStyleSheet
 class CSSStyleSheet {
-  constructor() { }
+  insertRule() { }
+  deleteRule() { }
   replace() { }
   replaceSync() { }
 }
