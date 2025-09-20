8267768d353b73474f334e96c2e4e28b7772bf02
Owen Buckley <owenbuckley13@gmail.com>
2025-09-20 18:02:58 -0400

    correct export types and functions

--- src/index.d.ts
+++ src/index.d.ts
@@ -6,14 +6,17 @@ export type Metadata = {
   }
 }
 
-export type renderToString = (elementURL: URL, wrappingEntryTag?: boolean, props?: any) => Promise<{
+type renderToString = (elementURL: URL, wrappingEntryTag?: boolean, props?: any) => Promise<{
   html: string;
   metadata: Metadata
 }>
 
-export type renderFromHTML = (html: string, elementURLs: URL[]) => Promise<{
+type renderFromHTML = (html: string, elementURLs: URL[]) => Promise<{
   html: string;
   metadata: Metadata
 }>
 
-declare module "wc-compiler" { }
+declare module "wc-compiler" {
+  export const renderToString: renderToString;
+  export const renderFromHTML: renderFromHTML;
+}
