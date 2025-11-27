e9055c6ff393d3d34ae3348a9416594d33271511
Owen Buckley <owenbuckley13@gmail.com>
2025-11-26 20:02:14 -0500

    docs: header navigation

--- docs/globals.d.ts
+++ docs/globals.d.ts
@@ -2,3 +2,8 @@ declare module '*.module.css' {
   const styles: Readonly<Record<string, string>>;
   export default styles;
 }
+
+declare module '*?type=raw' {
+  const content: string;
+  export default content;
+}
