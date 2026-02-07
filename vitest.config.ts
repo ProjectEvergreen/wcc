e533d2ef10b4b2935a804d1af8387bff6e55e403
Owen Buckley <owenbuckley13@gmail.com>
2026-02-06 20:02:01 -0500

    chore: vanilla web component browser test

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vitest/config';
+import { playwright } from '@vitest/browser-playwright';
 
 export default defineConfig({
   test: {
     include: ['./docs/**/*.test.ts'],
+    browser: {
+      provider: playwright(),
+      enabled: true,
+      headless: true,
+      instances: [{ browser: 'chromium' }],
+    },
   },
 });
