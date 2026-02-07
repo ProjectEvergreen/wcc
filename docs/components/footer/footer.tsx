4c22e2d98ba00c7e1880c2a29107d612e6d5533a
Owen Buckley <owenbuckley13@gmail.com>
2026-02-06 20:59:37 -0500

    chore: full component testing example

--- docs/components/footer/footer.tsx
+++ docs/components/footer/footer.tsx
@@ -13,6 +13,7 @@ export default class Footer extends HTMLElement {
         <a href="/" title="WCC Home Page" class={styles.logoLink}>
           {wccLogo}
         </a>
+
         <div class={styles.socialTray}>
           <wcc-social-tray></wcc-social-tray>
         </div>
