f4806c5fe3200bda43433bf9701fbc9686779cfc
Owen Buckley <owenbuckley13@gmail.com>
2026-04-18 19:27:04 -0400

    chore: formatting

--- test/cases/tsx/src/counter.tsx
+++ test/cases/tsx/src/counter.tsx
@@ -34,11 +34,9 @@ export default class Counter extends HTMLElement {
           {' '}
           - (function reference)
         </button>
-        {/* TODO: https://github.com/ProjectEvergreen/wcc/issues/88 */}
-        {/* @ts-expect-error - onclick should be.a function, but we coerce to an assignment */}
-        <button id="evt-assignment" onclick={(this.count -= 1)}>
-          {' '}
-          - (inline state update)
+        { /* TODO: https://github.com/ProjectEvergreen/wcc/issues/88 */ }
+        { /* @ts-expect-error - onclick should be.a function, but we coerce to an assignment */ }
+        <button id="evt-assignment" onclick={this.count -= 1}>{' '} - (inline state update)
         </button>
         <span>
           You have clicked{' '}
@@ -47,8 +45,8 @@ export default class Counter extends HTMLElement {
           </span>{' '}
           times
         </span>
-        {/* TODO: https://github.com/ProjectEvergreen/wcc/issues/88 */}
-        {/* @ts-expect-error - onclick should be.a function, but we coerce to an assignment */}
+        { /* TODO: https://github.com/ProjectEvergreen/wcc/issues/88 */ }
+        { /* @ts-expect-error - onclick should be.a function, but we coerce to an assignment */ }
         <button onclick={(this.count += 1)}> + (inline state update)</button>
         <button onclick={this.increment}> + (function reference)</button>
       </div>
